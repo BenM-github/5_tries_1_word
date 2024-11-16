4e9dae09e5967e24e7d14135c9cbb82c23d9a32f
Mueller <ben.mueller@zeiss.com>
2024-11-16 13:40:27 +0100

    renamed repo to private_5_tries_1_word

---
+++ app.js
@@ -0,0 +1,11 @@
+
+// === input ===
+import input from "./lib/input.js"
+
+
+// === get document elements ===
+const el_letter_input = document.getElementById('input')
+
+
+// === main ===
+input.setup(el_letter_input)
