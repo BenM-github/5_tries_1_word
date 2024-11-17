413290638a6608bb6bac65189afb10145c2024fc
Mueller <ben.mueller@zeiss.com>
2024-11-17 16:03:33 +0100

    modified word list and setup base display functions

--- app.js
+++ app.js
@@ -1,12 +1,46 @@
 
 // === input ===
-import input from "./lib/input.js"
+import Input from "./lib/input.js" // manages input of users
+import Field from "./lib/field.js" // manages display of tried words 
+import Suggestions from "./lib/suggestions.js"
 
 
 // === get document elements ===
 const el_input = document.getElementById('input') // input field
 const el_field = document.getElementById('field') // field / display
+const el_suggestions = document.getElementById('suggestions') // suggestions field
+
+
+// === settings ===
+const NUM_OF_LETTERS = 5
+
+// to get the word list we have to use async functions cause it takes some time to extract.
+async function get_data() {    
+    return fetch('../ngerman')
+        .then((rawData) => rawData.text())
+        .then((data) => data.split("\n"))
+}
+
 
 // === main ===
-input.setup(el_input) // setup input
-//display.setup(el_field) // setup display
+const input = new Input(el_input)
+
+// setup keyboard input
+document.addEventListener('keydown', function(e) {
+    let char = e.key.toUpperCase()
+
+    if (input.ALPHABET.includes(char)) { alert(char) }
+    else if (char == 'BACKSPACE') { alert('remove') }
+    else {
+        console.log(char);
+    }
+})
+
+const field = new Field(el_field, NUM_OF_LETTERS)
+
+// cause async functions can only run in to top level module we have to fetch the foundset here 
+const suggestions = new Suggestions(el_suggestions, await get_data(), NUM_OF_LETTERS)
+
+let pattern = []
+let x = 0
+let y = 0
